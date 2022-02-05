11a8f349dc358cc3e0ddc76c1c8fdd1501a0e51e
Diego Pineda <dpineda@serempre.com>
2022-02-04 22:05:18 -0500

    Ready to publish 0.0.1

---
+++ test/data/products.ts
@@ -0,0 +1,11 @@
+export const product1 = {
+  id: '1',
+  title: 'Coffee Mug - No Image',
+  // img: "./coffee-mug.png"
+}
+
+export const product2 = {
+  id: '2',
+  title: 'Coffee Mug - Image',
+  img: "./coffee-mug.png"
+}
